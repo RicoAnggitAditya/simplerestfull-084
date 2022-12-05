994ac431fb3771ad4f5084d91de3dd2555f68f25
RicoAnggitAditya <ricoanggit01@gmail.com>
2022-12-05 20:07:33 +0700

    Praktikum 10, Membuat RestFull API

---
+++ Documents/NetBeansProjects/Praktikum GET, PUT, POST dan Delete/src/main/java/simplerestfull/simplerestfull/Product.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplerestfull.simplerestfull;
+
+/**
+ *
+ * @author acer
+ */
+public class Product {
+    
+    private String Id;
+    private String Name;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    
+}
---
+++ Documents/NetBeansProjects/Praktikum GET, PUT, POST dan Delete/src/test/java/simplerestfull/simplerestfull/SimplerestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package simplerestfull.simplerestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplerestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
