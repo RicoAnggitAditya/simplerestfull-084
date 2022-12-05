6c7ffeb458a8fce241687531f545291591c44b5f
RicoAnggitAditya <ricoanggit01@gmail.com>
2022-12-05 20:33:35 +0700

    Method GET, POST, PUT dan DELETE

---
+++ Documents/NetBeansProjects/SimpleRestFull/src/test/java/restfull_API/simplerestfull/SimplerestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package restfull_API.simplerestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplerestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
