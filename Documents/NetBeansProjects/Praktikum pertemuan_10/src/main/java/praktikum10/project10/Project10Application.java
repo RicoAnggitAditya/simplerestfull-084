6a012a42d214c695b0ee6b79cd0242db1f770312
RicoAnggitAditya <ricoanggit01@gmail.com>
2022-12-05 20:19:34 +0700

    Method GET, POST, PUT dan DELETE

---
+++ Documents/NetBeansProjects/Praktikum pertemuan_10/src/main/java/praktikum10/project10/Project10Application.java
@@ -0,0 +1,13 @@
+package praktikum10.project10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project10Application.class, args);
+	}
+
+}
