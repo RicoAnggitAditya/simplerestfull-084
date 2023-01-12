c2985bcba9efa1cf021334cc7077879fe0c36749
RicoAnggitAditya <ricoanggit01@gmail.com>
2023-01-12 18:25:59 +0700

    Simple ResfFull

---
+++ src/main/java/simplerestfull/simpleresfull/SimpleresfullApplication.java
@@ -0,0 +1,13 @@
+package simplerestfull.simpleresfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleresfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleresfullApplication.class, args);
+	}
+
+}
